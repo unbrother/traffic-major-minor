d99c3f0e1ee859a8cbd37f2b6e466c92bc8a2275
Malcolm Morgan <mem48@users.noreply.github.com>
2020-06-24 12:16:50 +0100

    testing using zone based routing

--- R/02b_matchmajorminor.R
+++ R/02b_matchmajorminor.R
@@ -8,6 +8,7 @@ library(sf)
 library(dodgr)
 library(tmap)
 library(dplyr)
+library(concaveman)
 tmap_mode("view")
 
 
@@ -78,13 +79,39 @@ nearst_junction <- unlist(nearst_junction)
 osm_minor$nearst_junction <- nearst_junction
 qtm(osm_minor, lines.col = "nearst_junction", lines.lwd = 3) # plot each road colored by it nearest junction
 
-# Do the routing again and aggregate flows
-from = osm_minor$nearst_junction
-to = minor_cent$from_id
-to = to[!is.na(from)]
-from = from[!is.na(from)]
+# Make subgraphs
+road_cut <- st_cast(osm_major$geom, "LINESTRING")
 
-# Simiply the matrix as dodgr_flows_aggregate doesn't support pairwise
+minor_points <- st_cast(osm_minor$geom, "POINT")
+minor_points <- st_transform(minor_points, 27700)
+all_points <- st_cast(osm$geom, "POINT")
+
+minor_hull <- concaveman::concaveman(st_as_sf(all_points), concavity = 2)
+minor_hull <- st_make_valid(minor_hull)
+minor_hull <- st_collection_extract(minor_hull)
+
+# qtm(minor_hull) +
+#   qtm(osm_major)
+
+zones <- lwgeom::st_split(minor_hull, road_cut)
+zones <- st_collection_extract(zones)
+zones <- st_as_sf(zones)
+zones_inter <- st_contains_properly(zones, minor_points)
+zones$npoints <- lengths(zones_inter)
+zones <- zones[zones$npoints > 5, ]
+zones$id <- 1:nrow(zones)
+zones <- st_transform(zones, 4326)
+qtm(zones, fill = "id")
+
+graphs <- list()
+
+minor_cent <- st_as_sf(minor_cent, coords = c("X","Y"), crs = 4326, remove = FALSE)
+
+# Free up memmory
+rm(road_cut, points, zones_inter, both_int,minor_int, minor_hull,
+   minor_points, all_points, dists, traffic, graph, graph_ids, nearst_junction)
+gc()
+stop()
 
 od_to_odmatrix <- function(flow, attrib = 3, name_orig = 1, name_dest = 2) {
   out <- matrix(
@@ -96,19 +123,48 @@ od_to_odmatrix <- function(flow, attrib = 3, name_orig = 1, name_dest = 2) {
   out
 }
 
-flow = od_to_odmatrix(data.frame(from = from, to = to, flow = 1))
-# flow2 = flow
-# flow2[is.na(flow2)] <- 0
-# dodgr_flows_aggregate  now seems to work on Windows
-agg_flow <- dodgr_flows_aggregate(graph,
-                                  from = row.names(flow),
-                                  to = colnames(flow),
-                                  flows = flow,
-                                  contract = TRUE, quiet = FALSE, tol = 0,
-                                  norm_sums = FALSE)
-summary(agg_flow$flow)
-graph_undir <- merge_directed_graph(agg_flow)
-geoms <- dodgr_to_sfc (graph_undir)
+graphs <- list()
+for(i in zones$id){
+  message(paste0("Doing Zone ",i))
+  zone_sub <- st_buffer(zones[zones$id == i, ], 0.0001)
+  osm_sub <- osm_minor[zone_sub, , op = st_within]
+  cents_sub <- minor_cent[zone_sub, , op = st_within]
+  # qtm(zone_sub) +
+  #   qtm(osm_sub) +
+  #   qtm(cents_sub)
+  if(nrow(osm_sub) > 0){
+    graph_sub <- weight_streetnet(osm_sub, wt_profile = "motorcar")
+    pts = graph_sub[,c("from_id","component")]
+    pts = unique(pts)
+    cents_sub = left_join(cents_sub, pts, "from_id")
+    cents_sub = st_drop_geometry(cents_sub)
+    cents_sub = cents_sub[,c("from_id","component")]
+    flow = cents_sub[rep(1:nrow(cents_sub), times = nrow(cents_sub)),]
+    flow = cbind(flow, cents_sub[rep(1:nrow(cents_sub), each = nrow(cents_sub)),])
+    names(flow) = c("from_id","component1","to_id","component2")
+    flow$flow <- ifelse(flow$component1 == flow$component2, 1, NA)
+    flow <- flow[,c("from_id","to_id","flow")]
+
+    # Simiply the matrix as dodgr_flows_aggregate doesn't support pairwise
+    flow = od_to_odmatrix(flow)
+    graph_sub <- dodgr_flows_aggregate(graph_sub,
+                                      from = pts$from_id,
+                                      to = pts$from_id,
+                                      flows = flow,
+                                      contract = TRUE, quiet = TRUE, tol = 0,
+                                      norm_sums = FALSE)
+    class(graph_sub) <- "data.frame"
+    graphs[[i]] <- graph_sub
+  }
+
+
+}
+
+graphs <- bind_rows(graphs)
+
+summary(graphs$flow)
+#graph_undir <- merge_directed_graph(graphs)
+geoms <- dodgr_to_sfc (graphs)
 # graph_cont <- dodgr_contract_graph (graph_undir)
 gsf <- sf::st_sf (geoms)
 qtm(gsf, lines.col = "dat.flow", lines.lwd = 3) # Relative flows number
---
+++ R/dodgr_tests.R
@@ -0,0 +1,43 @@
+library(dodgr)
+graph <- weight_streetnet (hampi)
+from <- sample (graph$from_id, size = 10)
+to <- sample (graph$to_id, size = 5)
+to <- to [!to %in% from]
+flows <- matrix(rep(1, length(from) * length(to)),
+                 nrow = length(from))
+
+graph1 <- dodgr_flows_aggregate(graph, from = from, to = to, flows = flows, heap = "FHeap", norm_sums = FALSE)
+graph2 <- dodgr_flows_aggregate(graph, from = from, to = to, flows = flows, heap = "BHeap")
+graph3 <- dodgr_flows_aggregate(graph, from = from, to = to, flows = flows, heap = "TriHeap")
+#crashes graph4 <- dodgr_flows_aggregate(graph, from = from, to = to, flows = flows, heap = "TriHeapExt")
+graph5 <- dodgr_flows_aggregate(graph, from = from, to = to, flows = flows, heap = "Heap23")
+
+identical(graph1, graph2) #TRUE, FALSE
+identical(graph1, graph3) #TRUE
+identical(graph1, graph5) #TRUE
+
+summary(graph1$flow[graph1$flow > 0])
+dodgr_flowmap(graph1)
+
+# Make a simple Tree in igraph
+library(igraph)
+graph_i <- make_tree(19,2,"undirected")
+plot(graph_i)
+
+# Convert to dodgr and make undirected
+graph <- as_data_frame(graph_i)
+graph2 <- graph[,2:1]
+names(graph2) <- c("from","to")
+graph <- rbind(graph, graph2)
+graph$d <- 1
+
+# Flow of 1 from all to all (same as edge betweenness)
+flows <- matrix(rep(1, 19 * 19), ncol = 19)
+graph <- dodgr_flows_aggregate(graph, from = 1:19, to = 1:19, flows = flows)
+graph$flow
+
+# Do edge betweeness in igraph
+E(graph_i)$between <- edge.betweenness(graph_i)
+E(graph_i)$between
+
+
