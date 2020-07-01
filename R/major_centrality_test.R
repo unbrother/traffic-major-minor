b7a70925e54469e9c40f9c5f9dc6f8d76052cd13
Malcolm Morgan <mem48@users.noreply.github.com>
2020-07-01 10:10:35 +0100

    join tweaks

--- R/major_centrality_test.R
+++ R/major_centrality_test.R
@@ -1,3 +1,6 @@
+osm <- st_transform(osm, 4326)
+osm_major <- st_transform(osm, 4326)
+
 major_centrality <- weight_streetnet(osm, wt_profile = "motorcar")
 major_centrality <- dodgr_centrality(major_centrality)
 major_centrality <- merge_directed_graph(major_centrality)
@@ -11,7 +14,7 @@ tm_shape(major_centrality) +
 graphs_major <- left_join(major_centrality,
                     st_drop_geometry(osm_major[,c("osm_id","aadt")]),
                     by = c("way_id" = "osm_id"))
-graphs_major <- graphs_major[graphs_major$highway %in% c("motorway","motowray_link","primary","primary_link","trunk","trunk_link"),]
+graphs_major <- graphs_major[graphs_major$highway %in% c("motorway","motoway_link","primary","primary_link","trunk","trunk_link"),]
 colnames(graphs_major)[which(names(graphs_major) == "aadt")] <- "major_aadt"
 
 
@@ -20,6 +23,6 @@ traffic_major <- traffic[!traffic$road %in% c("C","U"),]
 traffic_major <- st_buffer(traffic_major, 1000)
 traffic_major <- st_transform(traffic_major, 4326)
 
-traffic_major <- st_join(traffic_major, graphs_major)
+traffic_major <- st_join(graphs_major, traffic_major)
 
 qtm(traffic_major, lines.col = "aadt", lines.lwd = 3)
