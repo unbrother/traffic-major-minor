afb42483700739f278b630b31cb8548ee191e5b6
Jose Mendoza <mendozajose@outlook.com>
2020-08-20 19:00:38 -0500

    Leeds Model

---
+++ R/01a_getdataLeeds.R
@@ -0,0 +1,209 @@
+# 03 Getdata
+# Aim this script get the OSM data for the Leeds area, cleans it and assigns
+# AADT values where available
+
+# ################################################################# #
+#### LOAD LIBRARIES AND DEFINE CORE SETTINGS                     ####
+# ################################################################# #
+
+### Clear memory
+rm(list = ls())
+
+### Load libraries
+library(sf)
+library(tmap)
+library(osmdata)
+library(dplyr)
+library(dismo)
+library(deldir)
+
+tmap_mode("view")
+
+# ################################################################# #
+#### GET ROAD OSM DATA                                           ####
+# ################################################################# #
+
+### Get Roads
+q = opq(getbb("leeds, UK")) %>%
+  add_osm_feature(key = "highway")
+
+osm_raw = osmdata_sf(q = q)
+
+# ################################################################# #
+#### PREP INPUT DATA                                             ####
+# ################################################################# #
+
+###
+osm <- osm_raw$osm_lines
+points <- osm_raw$osm_points
+polys <- osm_raw$osm_polygons # needed for roundabouts
+
+polys <- polys[polys$highway %in% c("living_street","primary","primary_link",
+                                    "residential","secondary", "secondary_link",
+                                    "tertiary",  "tertiary_link", "unclassified"),]
+
+polys <- st_cast(polys, "LINESTRING")
+polys <- polys[,c("osm_id","name","ref","highway",
+                  "junction","maxspeed","geometry")]
+
+osm <- st_transform(osm, 27700)
+points <- st_transform(points, 27700)
+polys <- st_transform(polys, 27700)
+
+osm <- osm[,c("osm_id","name","ref","highway",
+              "junction","maxspeed","geometry")]
+
+### only intrested in roads not paths
+osm <- osm[osm$highway %in% c("living_street","primary","primary_link",
+                              "residential","secondary", "secondary_link",
+                              "tertiary",  "tertiary_link", "unclassified"),]
+
+osm <- rbind(osm, polys)
+rm(osm_raw, polys)
+qtm(osm)
+### Find Junctions, OSM Points are both nodes that make up lines/polygons, and objects e.g. shops
+### remove points that are not nodes on the line
+### node points have no tags
+
+col.names <- names(points)[!names(points) %in% c("osm_id","highway", "crossing", "crossing_ref","geometry")] # Get column names other than osm_id, and highway which is just for junction types, and crossing info which can be junction between cycle way and road
+points.sub <- points
+points <- points[,c("osm_id","highway")]
+points.sub <- as.data.frame(points.sub)
+points.sub$geometry <- NULL
+points.sub <- points.sub[,col.names]
+rowsum <- as.integer(rowSums(!is.na(points.sub)))
+
+rm(points.sub, col.names)
+
+points <- points[rowsum == 0,] #Remove points with any tags
+
+### now check highway tag to remove things like traffic lights
+points <- points[is.na(points$highway) | points$highway %in% c("mini_roundabout","motorway_junction"), ]
+points <- points[,c("osm_id","geometry")]
+
+### Look for points that intersect lines
+inter <- st_intersects(points,osm)
+len <- lengths(inter)
+points <- points[len >= 2,] #Only keep points that intersec at least 2 lines i.e. a junction
+
+### Remove any duplicated points
+points <- points[!duplicated(points$geometry),]
+rm(len, rowsum, inter)
+
+# ################################################################# #
+#### GET AADT COUNTS                                             ####
+# ################################################################# #
+
+### Bounds and traffic
+#bounds <- readRDS("data/bounds.Rds")
+traffic_leeds <- read.csv("data/traffic_leeds.csv")
+traffic_leeds <- st_as_sf(traffic_leeds, coords = c("easting", "northing"),  crs = 27700)
+traffic_leeds <- traffic_leeds[traffic_leeds$year == max(traffic_leeds$year),]
+traffic_leeds <- traffic_leeds[,c("road_name","all_motor_vehicles")]
+colnames(traffic_leeds) <- c("road", "aadt", "geometry")
+#traffic <- traffic[bounds,] #SUBSET??
+qtm(traffic_leeds)
+
+
+### Subset major and minor roads
+#osm <- osm[bounds,]
+osm_major <- osm[osm$highway %in% c("motorway","motorway_link","primary","primary_link","trunk","trunk_link"),]
+osm_minor <- osm[!osm$highway %in% c("motorway","motorway_link","primary","primary_link","trunk","trunk_link"),]
+
+
+osm_major_cents <- st_coordinates(st_centroid(osm_major))
+
+nn = RANN::nn2(osm_major_cents, k = 20) ### TRY TO FIX THIS LOOP ###
+
+for(k in 1:2){
+  for(i in 1:nrow(osm_major)){
+    if(is.na(osm_major$ref[i])){
+      for(j in 2:20){
+        idx <- nn$nn.idx[i,j]
+        if(osm_major$highway[idx] == osm_major$highway[i]){
+          if(!is.na(osm_major$ref[idx])){
+            osm_major$ref[i] <- osm_major$ref[idx]
+            break
+          }
+        }
+      }
+    }
+  }
+}
+
+qtm(osm_major, lines.col = "ref", lines.lwd = 3)
+#rm(nn,osm_major_cents)
+
+osm <- rbind(osm_major, osm_minor)
+
+# Assign Traffic Counts to the roads --------------------------------------
+
+get.aadt.class <- function(e){
+  #message(paste0("doing ",e))
+  traffic.sub <- traffic.class[traffic.class$road == roadnames[e],]
+  traffic.sub <- traffic.sub[!duplicated(traffic.sub$geometry),]
+  osm.sub <- osm.nona[osm.nona$ref == roadnames[e],]
+
+  ### need at least 2 points to make voronoi polygons
+  if(nrow(traffic.sub) > 1){
+    ### Make voronoi polygons and convert to SF
+    # voronoi <- dismo::voronoi(xy = st_coordinates(traffic.sub),
+    #                           ext = st_bbox(osm.sub))
+    # voronoi <- as(voronoi, "sf")
+    # st_crs(voronoi) <- st_crs(traffic.sub)
+    voronoi <- st_voronoi(st_combine(traffic.sub))
+    voronoi <- st_collection_extract(voronoi)
+    voronoi <- st_as_sf(voronoi)
+
+  }else{
+    ### Make a big buffer around the point
+    voronoi <- st_buffer(traffic.sub, 500)
+  }
+
+  voronoi <- st_join(voronoi, traffic.sub)
+
+  ### Find Intersections of roads with vernoi polygons
+  inter <- st_intersects(osm.sub,voronoi)
+  ### Get aadt and ncycle values
+  osm.sub$aadt <- lapply(1:nrow(osm.sub),function(x){as.numeric(round(mean(traffic.sub$aadt[inter[[x]]])),0)})
+
+  ### Remove Unneeded Data
+  osm.sub <- as.data.frame(osm.sub)
+  osm.sub <- osm.sub[,c("osm_id","aadt")]
+
+  return(osm.sub)
+}
+
+
+### Separate Classified and Unclassified Roads
+traffic.class <- traffic_leeds[!substr(traffic_leeds$road,1,1) %in% c("U","C"),]
+traffic.unclass <- traffic_leeds[substr(traffic_leeds$road,1,1) %in% c("U","C"),]
+
+
+### Start with the classified
+roadnames <- unique(traffic.class$road)
+roadnames <- roadnames[roadnames %in% osm$ref]
+osm.nona <- osm[!is.na(osm$ref),] #Create a working dataset without nas
+osm.nona <- osm.nona[,c("osm_id","ref")] #Dump unneeded data
+res.class <- lapply(1:length(roadnames),get.aadt.class)
+res.class <- do.call("rbind",res.class)
+res.class <- res.class[!is.na(res.class$osm_id),]
+rm(osm.nona,roadnames)
+
+
+### remove any duplicates
+res.class <- res.class[!duplicated(res.class$osm_id),]
+res.class$aadt <- as.numeric(res.class$aadt)
+
+### Join onto the original osm data
+osm <- left_join(osm,res.class, by = c("osm_id" = "osm_id"))
+rm(res.class)
+
+qtm(osm[osm$highway %in% c("motorway","motorway_link","primary","primary_link","trunk","trunk_link"),]
+    , lines.lwd = 3, lines.col = "aadt")
+
+# Save these for later ----------------------------------------------------
+
+st_write(osm, "data/leeds_osm_all.gpkg", delete_dsn = TRUE)
+st_write(points, "data/leeds_points.gpkg", delete_dsn = TRUE)
+st_write(traffic_leeds, "data/leeds_traffic_points.gpkg", delete_dsn = TRUE)
---
+++ R/02_dataprocessing.R
@@ -0,0 +1,289 @@
+# Aim: To calculate AADT for minor roads
+# This script assumes the data has already been cleaned and the AADT for major roads assigned
+# Rework of original file dropping the splitting an buffering stage
+
+# Load Packages -----------------------------------------------------------
+
+library(ggplot2)
+library(sf)
+library(dodgr)
+library(tmap)
+library(dplyr)
+library(concaveman)
+tmap_mode("view")
+
+
+# Load Data ---------------------------------------------------------------
+
+osm = st_read("data/iow_osm_all.gpkg")
+points = st_read("data/iow_points.gpkg")
+traffic = st_read("data/iow_traffic_points.gpkg")
+
+# Find Junctions between minor and major roads ----------------------------
+
+osm_major <- osm[osm$highway %in% c("motorway","motowray_link","primary",
+                                    "primary_link","trunk","trunk_link"),]
+osm_minor <- osm[!osm$highway %in% c("motorway","motowray_link","primary",
+                                     "primary_link","trunk","trunk_link"),]
+
+minor_int <- st_intersects(points, osm_minor)
+major_int <- st_intersects(points, osm_major)
+
+minor_int = lengths(minor_int)
+major_int = lengths(major_int)
+both_int = ifelse(minor_int > 0 & major_int > 0, TRUE, FALSE)
+
+junc_majmi = points[both_int,]
+
+# Match Major Road AADT onto junctions
+junc_majmi <- st_join(junc_majmi, osm_major[,"aadt"])
+junc_majmi <- junc_majmi[!duplicated(junc_majmi$geom),]
+qtm(junc_majmi, dots.col = "aadt")
+
+qtm(traffic, dots.col = "aadt")
+
+# Convert to 4326 for dodgr
+osm_minor <- st_transform(osm_minor, 4326)
+junc_majmi <- st_transform(junc_majmi, 4326)
+
+# 5) Get mid-point of minor roads, i.e. centroid on the line
+minor_cent <- as.data.frame(st_coordinates(osm_minor))
+minor_cent <- group_by(minor_cent, L1) %>%
+  summarise(X = nth(X, n()/2),
+            Y = nth(Y, n()/ 2))
+
+# 6) Make dodgr graph of minor roads
+graph <- weight_streetnet(osm_minor, wt_profile = "motorcar")
+graph_ids <- graph[,c("from_id","from_lon","from_lat")]
+graph_ids <- unique(graph_ids)
+
+junc_majmi <- cbind(junc_majmi, st_coordinates(junc_majmi))
+junc_majmi <- left_join(junc_majmi, graph_ids, by = c("X" = "from_lon",
+                                                      "Y" = "from_lat"))
+minor_cent <- left_join(minor_cent, graph_ids, by = c("X" = "from_lon",
+                                                      "Y" = "from_lat"))
+
+# For each minor road centroid, find the nearest (in time) junction  --------
+dists <- dodgr_times(graph,
+                     from = junc_majmi$from_id,
+                     to = minor_cent$from_id,
+                     shortest = FALSE)
+
+nearst_junction <- list()
+for(i in 1:ncol(dists)){
+  sub <- dists[,i]
+  sub <- sub[!is.na(sub)]
+  if(length(sub) == 0){
+    nearst_junction[[i]] <- NA
+  } else {
+    mindist <- names(sub)[sub == min(sub, na.rm = TRUE)]
+    if(length(mindist) == 0){
+      mindist <- NA
+    }
+    if(length(mindist) > 1){
+      mindist <- mindist[1]
+    }
+    nearst_junction[[i]] <- mindist
+  }
+
+}
+nearst_junction <- unlist(nearst_junction)
+
+osm_minor$nearst_junction <- nearst_junction
+osm_minor$major_aadt <- junc_majmi$aadt[match(osm_minor$nearst_junction,
+                                              junc_majmi$from_id)]
+# plot each road colored by the AADT on the nearest (in time) major road
+qtm(osm_minor, lines.col = "major_aadt", lines.lwd = 3)
+
+
+
+# Split the minor roads into zones divided by the major road network --------
+
+# Make subgraphs
+road_cut <- st_cast(osm_major$geom, "LINESTRING")
+
+minor_points <- st_cast(osm_minor$geom, "POINT")
+minor_points <- st_transform(minor_points, 27700)
+all_points <- st_cast(osm$geom, "POINT")
+
+minor_hull <- concaveman::concaveman(st_as_sf(all_points), concavity = 2)
+minor_hull <- st_make_valid(minor_hull)
+minor_hull <- st_collection_extract(minor_hull)
+
+zones <- lwgeom::st_split(minor_hull, road_cut)
+zones <- st_collection_extract(zones)
+zones <- st_as_sf(zones)
+zones_inter <- st_contains_properly(zones, minor_points)
+zones$npoints <- lengths(zones_inter)
+zones <- zones[zones$npoints > 5, ]
+zones$id <- 1:nrow(zones)
+zones <- st_transform(zones, 4326)
+
+# plot the zones created
+qtm(zones, fill = "id")
+
+minor_cent <- st_as_sf(minor_cent, coords = c("X","Y"), crs = 4326,
+                       remove = FALSE)
+
+# Free up memory
+rm(road_cut, points, zones_inter, both_int,minor_int, minor_hull,
+   minor_points, all_points, dists, graph, graph_ids, nearst_junction,
+   major_int, mindist, sub)
+gc()
+
+
+# Loop over each zone and find the centrality of the minor road ne --------
+graphs <- list()
+
+for(i in zones$id){
+  message(paste0("Doing Zone ",i))
+  zone_sub <- st_buffer(zones[zones$id == i, ], 0.0001)
+  osm_sub <- osm_minor[zone_sub, , op = st_within]
+  cents_sub <- minor_cent[zone_sub, , op = st_within]
+  # qtm(zone_sub) +
+  #   qtm(osm_sub) +
+  #   qtm(cents_sub)
+  if(nrow(osm_sub) > 0){
+    graph_sub <- weight_streetnet(osm_sub, wt_profile = "motorcar")
+    graph_sub <- dodgr_centrality(graph_sub)
+    graph_sub <- merge_directed_graph(graph_sub)
+    clear_dodgr_cache()
+    graph_sub <- dodgr_to_sf(graph_sub)
+    #graph_sub <- dodgr_central(graph_sub)
+    graphs[[i]] <- graph_sub
+  }
+
+
+}
+
+graphs <- bind_rows(graphs)
+
+# Plot the centrality of all minor roads
+summary(graphs$centrality)
+
+tm_shape(graphs) +
+  tm_lines(col = "centrality", lwd = 3, style = "jenks")
+
+
+
+# Match Up centrality with major aadt -----------------------------------------
+summary(unique(graphs$way_id) %in% unique(osm_minor$osm_id))
+summary(duplicated(graphs$way_id)) # some osm_ids have been split
+
+graphs <- left_join(graphs,
+                       st_drop_geometry(osm_minor[,c("osm_id","major_aadt")]),
+                       by = c("way_id" = "osm_id"))
+
+# Graphs is a sf df of minor roads with a major_aadt and centrality --------
+head(graphs)
+
+
+# Compare measured AADT and centrality ------------------------------------
+traffic_minor <- traffic[traffic$road %in% c("C","U"),]
+# Buffer as traffic points are away from roads
+traffic_minor <- st_buffer(traffic_minor, 33)
+traffic_minor <- st_transform(traffic_minor, 4326)
+
+traffic_minor <- st_join(traffic_minor, graphs)
+summary(is.na(traffic_minor$centrality))
+
+# some simple plots
+plot(traffic_minor$centrality, traffic_minor$aadt)
+abline(0,1, col = "red")
+plot(traffic_minor$major_aadt, traffic_minor$aadt)
+
+traffic_minor %>% ggplot(aes(x = aadt, y = centrality)) +
+  geom_point(aes(colour = highway)) +
+  geom_smooth(method="lm", se=F, fullrange=FALSE, level=0.95)
+
+traffic_minor %>% ggplot(aes(x = aadt, y = major_aadt)) +
+  geom_point(aes(color = highway)) +
+  geom_smooth(method="lm", se=F, fullrange=FALSE, level=0.95)
+
+traffic_minor %>% ggplot(aes(x = centrality, y = major_aadt)) +
+  geom_point(aes(color = highway)) +
+  geom_smooth(method="lm", se=F, fullrange=FALSE, level=0.95)
+
+cor(traffic_minor$centrality, traffic_minor$aadt)
+cor(traffic_minor$major_aadt, traffic_minor$aadt)
+
+# simple model
+m1 <- lm(aadt ~ centrality + major_aadt, data = traffic_minor)
+summary(m1)
+plot(traffic_minor$aadt[!is.na(traffic_minor$aadt)], predict(m1),
+     main = "Model m1 observed vs predicted results",
+     ylab = "Predicted AADT",
+     xlab= "Observed AADT")
+abline(0,1, col = "red")
+cor(traffic_minor$aadt[!is.na(traffic_minor$centrality)], predict(m1))
+
+# log model
+m2 <- lm(aadt ~ log(centrality) + log(major_aadt), data = traffic_minor)
+summary(m2)
+plot(traffic_minor$aadt[!is.na(traffic_minor$aadt)], predict(m2),
+     main = "Model m2 observed vs predicted results",
+     ylab = "Predicted AADT",
+     xlab= "Observed AADT")
+abline(0,1, col = "red")
+cor(traffic_minor$aadt[!is.na(traffic_minor$centrality)], predict(m2))
+
+# Assign area type (urban and rural) ---------------------------------------
+
+# Import Strategi shp
+
+strategi <- st_read("data/strategi/urban_region.shp")
+strategi <- st_transform(strategi, 4326)
+
+# Classify as urban or rural
+
+traffic_areatype <- st_join(traffic_minor, strategi)
+traffic_areatype <- traffic_areatype %>%
+  mutate(areatype = ifelse(LEGEND %in% c("Large Urban Area polygon"), "urban",
+                           "rural"))
+
+# log model with added variable
+m2a <- lm(log(aadt) ~ log(centrality) + log(major_aadt) + areatype,
+          data = traffic_areatype)
+summary(m2a)
+plot(traffic_areatype$aadt[!is.na(traffic_areatype$centrality)],
+     exp(predict(m2a)),
+     main = "Model m2a observed vs predicted results",
+     ylab = "Predicted AADT",
+     xlab= "Observed AADT")
+abline(0,1, col = "red")
+cor(traffic_areatype$aadt[!is.na(traffic_areatype$centrality)],
+    exp(predict(m2a)))
+
+table(traffic_areatype$areatype)
+
+# log model poisson
+hist(traffic_areatype$aadt, breaks = 20,
+     main = "AADT distribution",
+     xlab = "AADT")
+
+m3 <- glm(aadt ~ log(centrality) + log(major_aadt) + areatype,
+          data = traffic_areatype, family = "poisson")
+summary(m3)
+plot(traffic_areatype$aadt[!is.na(traffic_areatype$centrality)],
+     exp(predict(m3)),
+     main = "Model m3 observed vs predicted results",
+     ylab = "Predicted AADT",
+     xlab= "Observed AADT")
+abline(0,1, col = "red")
+cor(traffic_areatype$aadt[!is.na(traffic_areatype$centrality)],
+    exp(predict(m3)))
+
+rmse3 <- sum(sqrt((exp(predict(m3))-traffic_minor$aadt)^2))/22
+
+data.frame(exp(predict(m3)), traffic_minor$aadt)
+
+par(mfrow = c(1, 1))
+plot(density(resid(m3, type='pearson')))
+plot(density(rstandard(m3, type='pearson')))
+
+plot(density(resid(m3, type='deviance')))
+
+scatter.smooth(predict(m3, type='response'), rstandard(mleed, type='deviance'), col='gray')
+
+par(mfrow = c(2, 2))
+plot(m3)
